680e7ae5786f6d6a33b60401c6971415690bdea2
adii1203 <aditya32ft@gmail.com>
2024-01-26 23:17:07 +0530

    Update CORS origin in app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: '20kb' }));
 app.use(
     cors({
-        origin: 'https://www.openurl.me',
+        origin: process.env.ORIGIN || 'http://127.0.0.1:5173',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     })
